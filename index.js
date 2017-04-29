f0970f0607f77f7a6c6918120c9114ddb786fd09
Alex <odmitrenko1@gmail.com>
2017-04-29 09:47:32 +0300

    Removed changes for other branches from index.js

--- index.js
+++ index.js
@@ -4,14 +4,12 @@ let bubbleSort = require('./bubble-sort');
 let coverByTile = require('./entry-test/coverByTile');
 let nextRound = require('./entry-test/nextRound');
 let coverByDominoes = require('./entry-test/coverByDominoes');
-let UnionFind = require('./entry-test/unionFind');
 
 let algolib = {
     bubbleSort,
 	coverByTile,
     nextRound,
     coverByDominoes,
-    UnionFind
 }
 
 module.exports = algolib;
