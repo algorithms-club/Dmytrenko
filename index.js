e20e6898a5d3400eac93f0ad655f3a9eece50999
AlexDoh <odmitrenko1@gmail.com>
2017-05-03 11:26:56 +0300

    Fixed index.js - added QuickUnion module

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ let bubbleSort = require('./bubble-sort');
 let coverByTile = require('./entry-test/coverByTile');
 let nextRound = require('./entry-test/nextRound');
 let coverByDominoes = require('./entry-test/coverByDominoes');
+let QuickUnion = require('./entry-test/quickUnion');
 
 let algolib = {
     bubbleSort,
 	coverByTile,
     nextRound,
     coverByDominoes,
+    QuickUnion
 }
 
 module.exports = algolib;
